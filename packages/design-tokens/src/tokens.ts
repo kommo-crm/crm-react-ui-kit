ef54b062ca551070a95efffd1208ab07a1325b69
kekovina <kekovinya@gmail.com>
2026-03-16 13:31:08 +0300

    fix: types in build

--- packages/design-tokens/src/tokens.ts
+++ packages/design-tokens/src/tokens.ts
@@ -1,16 +1,1 @@
 export { tokens } from './_tokens.generated';
-
-export type {
-  ColorValue,
-  Hex,
-  RGB,
-  RGBA,
-  HSL,
-  HSLA,
-  CSSVariable,
-  ColorFunction,
-  ColorKeyword,
-  ColorShades,
-  ScaledShades,
-} from '@/types/color';
-export type { Theme, ColorScale } from '@/const';
