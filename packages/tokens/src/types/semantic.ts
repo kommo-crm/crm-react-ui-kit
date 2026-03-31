cb0fd6f632bc0d5c1c61ac4c35ab2ffcafe339e6
kekovina <kekovinya@gmail.com>
2026-03-31 16:23:27 +0300

    feat: tokens

--- packages/tokens/src/types/semantic.ts
+++ packages/tokens/src/types/semantic.ts
@@ -1,9 +1,17 @@
-import type { ColorFunction, ColorKeyword, ColorValue } from '@/types/color';
+import type { ColorValue } from '@/types/color';
 import type { PrimitivePath } from '@/design/primitives';
 
 export type PaletteValue = PrimitivePath | ColorValue;
 
-export type PaletteBackground = 'base' | 'primary' | 'secondary' | 'error';
+export type PaletteBackground =
+  | 'default'
+  | 'default-disabled'
+  | 'base'
+  | 'primary'
+  | 'primary-disabled'
+  | 'secondary'
+  | 'error';
+
 export type PaletteForeground =
   | 'primary'
   | 'inverted'
@@ -15,11 +23,22 @@ export type PaletteForeground =
 export type PaletteBorder = 'medium' | 'strong' | 'error';
 export type PaletteOverlay = 'disabled';
 
+export type PaletteText =
+  | 'primary'
+  | 'primary-inverse'
+  | 'secondary-light'
+  | 'secondary-dark'
+  | 'secondary-dark-green';
+
+export type PaletteLink = 'primary';
+
 export type SemanticTokens = {
   palette: {
     background: Record<PaletteBackground, PaletteValue>;
     foreground: Record<PaletteForeground, PaletteValue>;
     border: Record<PaletteBorder, PaletteValue>;
     overlay: Record<PaletteOverlay, PaletteValue>;
+    text: Record<PaletteText, PaletteValue>;
+    link: Record<PaletteLink, PaletteValue>;
   };
 };
