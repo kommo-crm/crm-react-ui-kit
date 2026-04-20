a3e1f18e9bdab379907f7b16e6dd7edc97090445
kekovina <kekovinya@gmail.com>
2026-04-20 18:43:15 +0300

    feat: add token

--- packages/tokens/src/design/themes/alternativeUiKit/index.ts
+++ packages/tokens/src/design/themes/alternativeUiKit/index.ts
@@ -2,7 +2,13 @@ import { SemanticUiKitTokens } from '@/types/semantic';
 
 import { defineSemanticTokens, defineTheme } from '@/define';
 
-const semanticTokens = defineSemanticTokens<SemanticUiKitTokens>({});
+const semanticTokens = defineSemanticTokens<SemanticUiKitTokens>({
+  palette: {
+    text: {
+      error: 'color.dark.red.300',
+    },
+  },
+});
 
 export const alternativeUiKitTheme = defineTheme<SemanticUiKitTokens>({
   id: 'alternative-ui-kit',
--- packages/tokens/src/design/themes/lightUiKit/index.ts
+++ packages/tokens/src/design/themes/lightUiKit/index.ts
@@ -2,7 +2,13 @@ import { SemanticUiKitTokens } from '@/types/semantic';
 
 import { defineSemanticTokens, defineTheme } from '@/define';
 
-const semanticTokens = defineSemanticTokens<SemanticUiKitTokens>({});
+const semanticTokens = defineSemanticTokens<SemanticUiKitTokens>({
+  palette: {
+    text: {
+      error: 'color.light.red.600',
+    },
+  },
+});
 
 export const lightUiKitTheme = defineTheme<SemanticUiKitTokens>({
   id: 'light-ui-kit',
