e7e3b300e5774281b7a0078cc6fbc6926686d3e9
name <132576459+Lepas12ses@users.noreply.github.com>
2026-05-22 11:27:56 +0300

    feat(Ribbon): Move some values from styles to theme

--- src/components/Ribbon/Ribbon.themes.ts
+++ src/components/Ribbon/Ribbon.themes.ts
@@ -1,6 +1,11 @@
 type RibbonThemeKey =
   | '--crm-ui-kit-ribbon-background-color'
-  | '--crm-ui-kit-ribbon-color';
+  | '--crm-ui-kit-ribbon-color'
+  | '--crm-ui-kit-ribbon-height'
+  | '--crm-ui-kit-ribbon-font-size'
+  | '--crm-ui-kit-ribbon-line-height'
+  | '--crm-ui-kit-ribbon-font-weight'
+  | '--crm-ui-kit-ribbon-text-transform';
 
 export type RibbonTheme = {
   [K in RibbonThemeKey]: string;
@@ -9,4 +14,9 @@ export type RibbonTheme = {
 export const RibbonPrimaryTheme: RibbonTheme = {
   '--crm-ui-kit-ribbon-background-color': 'var(--crm-ui-kit-color-error)',
   '--crm-ui-kit-ribbon-color': 'var(--crm-ui-kit-color-white)',
+  '--crm-ui-kit-ribbon-height': '19px',
+  '--crm-ui-kit-ribbon-font-size': '10px',
+  '--crm-ui-kit-ribbon-line-height': '14px',
+  '--crm-ui-kit-ribbon-font-weight': '700',
+  '--crm-ui-kit-ribbon-text-transform': 'uppercase',
 };
