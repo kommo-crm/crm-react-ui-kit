ab4d2a882124aaabf8954eea851bf73e7b58fc50
Snoppei <negulyaev2017@gmail.com>
2026-05-26 16:53:29 +0300

    fix: badge types

--- src/components/Badge/Badge.props.ts
+++ src/components/Badge/Badge.props.ts
@@ -2,28 +2,17 @@ import { HTMLAttributes } from 'react';
 
 import { BadgeThemeType } from './Badge.themes';
 
-type BadgeBaseProps = HTMLAttributes<HTMLSpanElement> & {
+export interface BadgeProps extends HTMLAttributes<HTMLSpanElement> {
   /**
    * Object with CSS properties of the theme.
    */
   theme: BadgeThemeType;
-};
-
-type BadgeWithChildren = BadgeBaseProps & {
+  /**
+   * @deprecated Use children instead.
+   */
+  title?: string;
   /**
    * Badge text.
    */
-  children: string;
-  title?: never;
-};
-
-type BadgeWithTitle = BadgeBaseProps & {
-  /**
-   * @deprecated This prop will be removed in a future major version.
-   * Use children instead.
-   */
-  title: string;
-  children?: never;
-};
-
-export type BadgeProps = BadgeWithChildren | BadgeWithTitle;
+  children?: string;
+}
