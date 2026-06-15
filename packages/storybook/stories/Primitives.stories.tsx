1bd2207bb51f2542feb1d9342a65f479319aece5
kekovina <kekovinya@gmail.com>
2026-06-15 11:39:32 +0300

    fix: refactoring

--- packages/storybook/stories/Primitives.stories.tsx
+++ packages/storybook/stories/Primitives.stories.tsx
@@ -7,19 +7,24 @@ import { Appearance } from '@ui-kit/lib/appearance';
 
 import { PrimitivePalette } from '@storybook-utils/components';
 
+type ShadeTuple = [ColorShade, Token];
+
+type ShadeGroups = ReturnType<typeof makeGroups>;
+
 function makeGroups(palette: Palette) {
   return Object.entries(palette).map(([name, shades]) => ({
     name,
-    shades: (Object.entries(shades) as [ColorShade, Token][]).map(
-      ([shade, token]) => ({ shade, token })
-    ),
+    shades: (Object.entries(shades) as ShadeTuple[]).map(([shade, token]) => ({
+      shade,
+      token,
+    })),
   }));
 }
 
 const lightGroups = makeGroups(color.light);
 const darkGroups = makeGroups(color.dark);
 
-function PrimitivesPage({ groups }: { groups: ReturnType<typeof makeGroups> }) {
+function PrimitivesPage({ groups }: { groups: ShadeGroups }) {
   return (
     <div style={{ padding: 24 }}>
       <PrimitivePalette groups={groups} />
